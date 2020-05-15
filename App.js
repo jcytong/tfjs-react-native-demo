84f79720b693d072e08a9c611e465dc91d399fc3
Johnny Tong <hi@johnnytong.com>
2020-05-15 02:39:56 -0400

    Got tf ready in app

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Fragment } from 'react';
+import { Button, SafeAreaView, StyleSheet, View, Text, StatusBar } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
+
+//import { Diagnostic } from './components/diagnostic';
+//import { MobilenetDemo } from './components/mobilenet_demo';
+//import { TestRunner } from './components/tfjs_unit_test_runner';
+//import { WebcamDemo } from './components/webcam/webcam_demo';
+//import { RealtimeDemo } from './components/webcam/realtime_demo';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTfReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    // Wait for tf to be ready.
+    console.log('waiting...')
+    await tf.ready();
+    console.log('yeah baby...tf ready!')
+    // Signal to the app that tensorflow.js can now be used.
+    this.setState({
+      isTfReady: true,
+    });
+  }
+
+  render() {
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    });
+
+    return (<View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+      <Text>{this.state.isTfReady}</Text>
+      <Text>SO HM LO</Text>
+    </View>)
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
